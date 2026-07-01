ade07b4301e541f08fb658dd7de923028fb19e95
momtasirrafi <momtasir.rafi@gmail.com>
2026-07-01 17:03:44 +0600

    Replace

--- server.js
+++ server.js
@@ -1,27 +1,42 @@
 import express from "express";
 import mongoose from "mongoose";
 import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 
-app.use(express.static(process.cwd()));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log("✅ MongoDB Connected"))
-  .catch(err => console.log(err));
+// Serve static files
+app.use(express.static(__dirname));
 
+// Homepage
 app.get("/", (req, res) => {
-  res.sendFile(path.join(process.cwd(), "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+const User = mongoose.model("User", {
+  name: String,
+  email: String,
 });
 
 app.get("/users", async (req, res) => {
-  const User = mongoose.model("User", {
-    name: String,
-    email: String,
-  });
-
   const users = await User.find();
   res.json(users);
 });
 
+// IMPORTANT
 export default app;
+
+// Local only
+if (process.env.NODE_ENV !== "production") {
+  app.listen(3000, () => {
+    console.log("Running on http://localhost:3000");
+  });
+}
